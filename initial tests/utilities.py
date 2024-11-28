d880255732b0bced85d758899bdff6d17f28f196
HalRejali <dingusinc.com@gmail.com>
2024-11-28 13:14:47 +0000

    whats your message

--- initial tests/utilities.py
+++ initial tests/utilities.py
@@ -114,6 +114,104 @@ def prepare_data_2(test_size=0.2, accept_data_filename="l1calo_hist_EGZ_extended
 
     return X_train, X_test, y_train, y_test, X_train_all, X_test_all
 
+def prepare_data_3(test_size=0.2, accept_data_filename="l1calo_hist_EGZ_extended.root", reject_data_filename="l1calo_hist_ZMUMU_extended.root", data_subdir="ZMUMU_EGZ_extended_np_pd"):
+    save_path = os.path.join(os.path.pardir, "data", data_subdir)
+    if os.path.exists(os.path.join(save_path,"np_data.npz")) and os.path.exists(os.path.join(save_path,"input_df.parquet")):
+        print(f"found preprepared data in {save_path}")
+        np_data = np.load(os.path.join(save_path,"np_data.npz"))
+        input_np, labels_np = np_data["input_np"], np_data["labels_np"]
+        input_df = pd.read_parquet(os.path.join(save_path,"input_df.parquet"))
+
+
+    else:
+        print(f"preprepared data in {save_path} is missing, preparing and saving here")
+        accept_data_path= os.path.join(os.path.pardir, "data", accept_data_filename)
+        reject_data_path= os.path.join(os.path.pardir, "data", reject_data_filename)
+        DFs = import_data_files([accept_data_path, reject_data_path])
+
+        accepted_numpy = ak.to_numpy(DFs[0]['SuperCell_ET'])
+        rejected_numpy = ak.to_numpy(DFs[1]['SuperCell_ET'])
+        accepted_labels = np.ones(accepted_numpy.shape[0])
+        rejected_labels = np.zeros(rejected_numpy.shape[0])
+
+        accepted_df = pd.DataFrame({'offline_ele_pt': DFs[0]['offline_ele_pt'],'Label': 1})
+        rejected_df = pd.DataFrame({'offline_ele_pt': DFs[1]['offline_ele_pt'],'Label': 0})
+
+        input_np = np.concatenate((accepted_numpy, rejected_numpy), axis=0)
+        input_df = pd.concat([accepted_df,rejected_df]).reset_index(drop=True)
+        labels_np = np.concatenate((accepted_labels, rejected_labels), axis=0)
+
+
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+        np.savez(os.path.join(save_path,"np_data.npz"), input_np=input_np,labels_np=labels_np)
+        input_df.to_parquet(os.path.join(save_path,"input_df.parquet"), index=False)
+
+    X_train, X_test, pd_passthrough_train, pd_passthrough_test, y_train, y_test = train_test_split(input_np, input_df, labels_np, test_size=test_size, random_state=42)
+
+    return X_train, X_test, y_train, y_test, pd_passthrough_train, pd_passthrough_test
+
+def prepare_data_4(test_size=0.2, accept_data_filename="l1calo_hist_EGZ_extended.root", reject_data_filename="l1calo_hist_ZMUMU_extended.root", data_subdir="ZMUMU_EGZ_extended_np_pd",format_mode="SuperCell_ET"):
+    save_path = os.path.join(os.path.pardir, "data", data_subdir)
+    if os.path.exists(os.path.join(save_path,"np_data.npz")) and os.path.exists(os.path.join(save_path,"input_df.parquet")):
+        print(f"found preprepared data in {save_path}")
+        np_data = np.load(os.path.join(save_path,"np_data.npz"))
+        input_np, labels_np = np_data["input_np"], np_data["labels_np"]
+        input_df = pd.read_parquet(os.path.join(save_path,"input_df.parquet"))
+
+
+    else:
+        print(f"preprepared data in {save_path} is missing, preparing and saving here")
+        accept_data_path= os.path.join(os.path.pardir, "data", accept_data_filename)
+        reject_data_path= os.path.join(os.path.pardir, "data", reject_data_filename)
+        DFs = import_data_files([accept_data_path, reject_data_path])
+
+        accepted_labels = np.ones(DFs[0].shape[0])
+        rejected_labels = np.zeros(DFs[1].shape[0])
+        accepted_df = pd.DataFrame({'offline_ele_pt': DFs[0]['offline_ele_pt'],'Label': 1})
+        rejected_df = pd.DataFrame({'offline_ele_pt': DFs[1]['offline_ele_pt'],'Label': 0})
+
+        input_np = format_numpy_training_input(DFs,format_mode)
+        input_df = pd.concat([accepted_df,rejected_df]).reset_index(drop=True)
+        labels_np = np.concatenate((accepted_labels, rejected_labels), axis=0)
+        
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+        np.savez(os.path.join(save_path,"np_data.npz"), input_np=input_np,labels_np=labels_np)
+        input_df.to_parquet(os.path.join(save_path,"input_df.parquet"), index=False)
+
+    print(input_np.shape,input_np)
+    X_train, X_test, pd_passthrough_train, pd_passthrough_test, y_train, y_test = train_test_split(input_np, input_df, labels_np, test_size=test_size, random_state=42)
+
+    return X_train, X_test, y_train, y_test, pd_passthrough_train, pd_passthrough_test
+
+def format_numpy_training_input(DFs,format_mode):
+    if format_mode == "SuperCell_ET":
+        accepted_numpy = ak.to_numpy(DFs[0]['SuperCell_ET'])
+        rejected_numpy = ak.to_numpy(DFs[1]['SuperCell_ET'])
+        
+    elif format_mode == "iso_vars":
+        columns = ["eFEX_ReC", "eFEX_ReE", "eFEX_RhE", "eFEX_RhH", "eFEX_WsN", "eFEX_WsD"]
+        accepted_numpy = DFs[0][columns].to_numpy(dtype=np.float32)
+        rejected_numpy = DFs[1][columns].to_numpy(dtype=np.float32)
+
+    elif format_mode == "reduced_SuperCell_ET":
+        full_accepted_numpy = ak.to_numpy(DFs[0]['SuperCell_ET'])
+        full_rejected_numpy = ak.to_numpy(DFs[1]['SuperCell_ET'])
+        accepted_seed_indices = np.argmax(full_accepted_numpy[:, 49:52 + 1], axis=1) + 49
+        rejected_seed_indices = np.argmax(full_rejected_numpy[:, 49:52 + 1], axis=1) + 49
+
+        delete_indices = {
+        49: [24, 25, 26, 28, 29, 30, 57, 58, 59, 61, 62, 63, 90, 91, 92, 94, 95, 96],  # Indices to delete for 49
+        50: [1, 5, 25, 26, 29, 30, 34, 38, 58, 59, 62, 63, 67, 71, 91, 92, 95, 96],  # Indices to delete for 50
+        51: [1, 2, 5, 6, 26, 30, 34, 35, 38, 39, 59, 63, 67, 68, 71, 72, 92, 96],  # Indices to delete for 51
+        52: [1, 2, 3, 5, 6, 7, 34, 35, 36, 38, 39, 40, 67, 68, 69, 71, 72, 73]}   # Indices to delete for 52
+
+        accepted_numpy = np.array([np.delete(row, delete_indices[cond]) for row, cond in zip(full_accepted_numpy, accepted_seed_indices)])
+        rejected_numpy = np.array([np.delete(row, delete_indices[cond]) for row, cond in zip(full_rejected_numpy, rejected_seed_indices)])
+
+
+    return np.concatenate((accepted_numpy, rejected_numpy), axis=0)
 
 def plot_2D_TSNE(embedded_data, colour_var, title):
     plt.figure(figsize=(10, 6))
@@ -129,15 +227,24 @@ def plot_3D_TSNE(embedded_data, colour_var,point_size=2):
     fig.update_traces(marker=dict(line=dict(width=0),size=np.ones(colour_var.shape)*point_size))
     fig.show()
 
-def evaluate_sklearn_model(y_test, y_pred):
+def evaluate_sklearn_model(y_test, y_pred,show_CR=True,show_MSE=True,model_name=None):
+    if model_name != None:
+        print("Evaluation of "+model_name)
     print(f"Accuracy: {accuracy_score(y_test, y_pred):.4f}")
-    print("Classification Report:\n",classification_report(y_test, y_pred))
+    if show_CR:
+        print("Classification Report:\n",classification_report(y_test, y_pred))
     print("Confusion Matrix:\n",confusion_matrix(y_test, y_pred))
-    print("Mean Squared Error:\n",mean_squared_error(y_test, y_pred))
+    if show_MSE:
+        print("Mean Squared Error:\n",mean_squared_error(y_test, y_pred))
 
-# XX Works for SVC and SGB so far
 def compute_roc(model, X_test, y_test):
-    y_scores = model.decision_function(X_test)
+    if hasattr(model, "decision_function"):  # For models like SVM or SGD
+        y_scores = model.decision_function(X_test)
+    elif hasattr(model, "predict_proba"):  # For models like XGBoost or other tree-based models
+        y_scores = model.predict_proba(X_test)[:, 1]
+    else:
+        raise ValueError("Model must have either a decision_function or predict_proba method.")
+    
     fpr, tpr, _ = roc_curve(y_test, y_scores)
     roc_auc = auc(fpr, tpr)
     return fpr, tpr, roc_auc
@@ -152,15 +259,36 @@ def plot_roc(fpr, tpr, roc_auc):
     plt.legend(loc="lower right")
     plt.show()
 
-def plot_efficiency_vs_ele_PT(X_test_all, et_Low = 20, et_High = 60):
+def plot_efficiency_vs_ele_PT(X_test_all, et_Low = 20, et_High = 60,prediction_parameter="pred",title_string="model"):
     electrons_all,bins,_ = plt.hist( X_test_all.query("Label == 1")["offline_ele_pt"],bins=40,alpha=0.6,range=[et_Low,et_High])
-    electrons_all_tagged,bins,_ = plt.hist( X_test_all.query("Label == 1 & pred == 1")["offline_ele_pt"],bins=40,alpha=0.6,range=[et_Low,et_High])
+    electrons_all_tagged,bins,_ = plt.hist( X_test_all.query(f"Label == 1 & {prediction_parameter} == 1")["offline_ele_pt"],bins=40,alpha=0.6,range=[et_Low,et_High])
     plt.clf()
     electrons_efficiency = electrons_all_tagged/electrons_all
 
     plt.bar(bins[:-1], electrons_efficiency, width = np.diff(bins), align='edge', edgecolor='black')
-    plt.title('efficiency against electron PT')
+    plt.title(f'efficiency against electron PT for {title_string}')
     plt.xlabel('offline electron pt')
     plt.ylabel('Efficiency')
     plt.xlim(bins[0], bins[-1])
+    plt.show()
+
+def multi_roc(classifiers, X_test, y_test, classifiers_2=None, X_test_2=None, y_test_2=None):
+    plt.figure(figsize=(10, 10))
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    for classifier in classifiers:
+        model = classifiers[classifier]
+        fpr, tpr, roc_auc = compute_roc(model,X_test,y_test)
+        plt.plot(fpr, tpr, lw=1, label=classifier+f" ROC (AUC = {roc_auc:.4f})")
+
+    if classifiers_2 != None:
+        for classifier in classifiers_2:
+            model = classifiers_2[classifier]
+            fpr, tpr, roc_auc = compute_roc(model,X_test_2,y_test_2)
+            plt.plot(fpr, tpr, lw=1, label=classifier+f" ROC (AUC = {roc_auc:.4f})")
+
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig("multi_roc", dpi=400, bbox_inches='tight')
     plt.show()
